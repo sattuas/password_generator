a4ccea1f4367e2b371d4c40a4a8b675bce673cd9
raphael <rafael.dks10@gmail.com>
2025-11-15 01:26:48 -0300

    user confirm options

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from models.generator import PasswordGenerator
 
 
+def confirm_option(text):
+    option = ''
+    while True:
+        option = str(input(f'{text} [Y/n] ')).strip()[0]
+        if option in 'Yy':
+            return True
+        elif option in 'Nn':
+            return False
+
 print("===== PASSWORD GENERATOR ======")
+
+letter_upper = False
+letter_lower = False
+symbols = False
+numbers = False
 
 
 while True:
@@ -11,8 +25,13 @@ while True:
 
     match option:
         case 1:
+            letter_upper = confirm_option("capital letters?")
+            letter_lower = confirm_option("lower letters?")
+            symbols = confirm_option("symbols?")
+            numbers = confirm_option("numbers?")
+
             print("generating password...")
-            pw = PasswordGenerator(16, True, True, True, True)
+            pw = PasswordGenerator(16, letter_upper, letter_lower, symbols, numbers)
             print(f'generated password: {pw.generate()}')
         case 0:
             print("closing...")
