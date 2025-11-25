178b2726b7c13a67c7c13074dfc957eb434f32aa
raphael <rafael.dks10@gmail.com>
2025-11-24 22:00:35 -0300

    verifyng minimum and maximum characters on password

--- models/app.py
+++ models/app.py
@@ -8,10 +8,12 @@ class PasswordApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Password Generator")
-        self.root.geometry("500x320")
+        self.root.geometry("500x350")
         self.root.resizable(False, False)
 
         self.first_password_generated = False
+
+        self.size = tk.IntVar()
 
         self.uppercase = tk.IntVar(value=1)
         self.lowercase = tk.IntVar(value=1)
@@ -30,6 +32,14 @@ class PasswordApp:
 
         title = ttk.Label(self.root, text="GENERATE A PASSWORD", font=("Arial", 16, "bold"))
         title.pack(pady=15)
+
+        size_frame = ttk.Frame(self.root)
+        size_frame.pack(pady=(0, 10))
+
+        ttk.Label(size_frame, text="Size").pack(side="left")
+
+        self.size_entry = ttk.Spinbox(size_frame, from_=6, to=16, textvariable=self.size)
+        self.size_entry.pack(side="left")
 
         cb_frame = ttk.Frame(self.root)
         cb_frame.pack(pady=(0, 20))
@@ -64,13 +74,19 @@ class PasswordApp:
     def get_password(self):
         try:
             gen = PasswordGenerator(
-                16,
+                int(self.size_entry.get()),
                 self.uppercase.get(),
                 self.lowercase.get(),
                 self.symbols.get(),
                 self.digits.get()
             )
-            self.message.set(gen.generate())
+
+            if int(self.size.get()) > 16:
+                self.flash.set("Enter a maximum size of 16 characters")
+            elif int(self.size.get()) < 6:
+                self.flash.set("Enter a minimum size of 6 characters")
+            else:
+                self.message.set(gen.generate())
             
             if not self.first_password_generated:
                 self.copy_btn.configure(state="normal")
@@ -91,6 +107,10 @@ class PasswordApp:
 
     def open_link(self, event):
         webbrowser.open("https://github.com/sattuas")
+
+    def verify_size(self):
+        if self.size_entry > 16:
+            self.flash.set("max!")
 
         
 if __name__ == "__main__":
